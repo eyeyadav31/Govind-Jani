0c2ecef3b288db0ebf4e86391eebd7a09fa9a626
eyeyadav31 <eyeyadav31@gmail.com>
2025-04-15 17:36:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT",400))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT",9000))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
@@ -78,21 +78,21 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/gq8cyp.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/k17l19.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://files.catbox.moe/gq8cyp.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/k17l19.jpg"
 )
-PLAYLIST_IMG_URL = "https://files.catbox.moe/gq8cyp.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/gq8cyp.jpg"
-TELEGRAM_AUDIO_URL = "https://files.catbox.moe/gq8cyp.jpg"
-TELEGRAM_VIDEO_URL = "https://files.catbox.moe/gq8cyp.jpg"
-STREAM_IMG_URL = "https://files.catbox.moe/gq8cyp.jpg"
-SOUNCLOUD_IMG_URL = "https://files.catbox.moe/gq8cyp.jpg"
-YOUTUBE_IMG_URL = "https://files.catbox.moe/gq8cyp.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/gq8cyp.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/gq8cyp.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/gq8cyp.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/k17l19.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/k17l19.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/k17l19.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/k17l19.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/k17l19.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/k17l19.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/k17l19.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/k17l19.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/k17l19.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/k17l19.jpg"
 
 
 def time_to_seconds(time):
